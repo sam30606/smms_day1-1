614fc102d8996c534116ef60d1d67f85f3318097
sam30606 <sam30606@gmail.com>
2023-08-01 13:27:04 +0800

    [Dev Fix] Better method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,12 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get("helloworld")
+  @Get("/")
   getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get("helloworld")
+  returnHello(): string {
     return this.appService.getHello();
   }
 }
